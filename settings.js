1b77fa5703e25fe70f3a661d2bfe807cee94432d
Beliht <belizairepicard@gmail.com>
2024-09-05 00:33:44 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,20 +28,20 @@ global.link = 'https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g'
 global.prefa = ['','!','.','#','&']
 
 //false=disable and true=enable
-global.autoRecording = false //auto recording
-global.autoTyping = false //auto typing
-global.autorecordtype = false //auto typing + recording
-global.autoread = false //auto read messages
+global.autoRecording = true //auto recording
+global.autoTyping = true//auto typing
+global.autorecordtype = true//auto typing + recording
+global.autoread = true//auto read messages
 global.autobio = true //auto update bio
-global.anti92 = false //auto block +92 
-global.autoswview = false //auto view status/story
+global.anti92 = true //auto block +92 
+global.autoswview = true //auto view status/story
 
 //menu type 
 //v1 is image menu, 
 //v2 is link + image menu,
 //v3 is video menu,
 //v4 is call end menu
-global.typemenu = 'v1'
+global.typemenu = 'v2'
 
 //text bug
 global.xbugtex = {
